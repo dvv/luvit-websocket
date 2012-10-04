02b74c14fe547a32e858c56717af78757076e258
Vladimir Dronnikov <dronnikov@gmail.com>
2012-10-04 09:45:02 +0400

    cleanup

--- src/hybi10.c
+++ src/hybi10.c
@@ -25,7 +25,6 @@
 static int encode(lua_State *L) {
 
   uint8_t *p = (uint8_t *)luaL_checkstring(L, 1);
-  // FIXME: checklong?
   uint32_t len = luaL_checkint(L, 2);
   uint32_t mask = luaL_checkint(L, 3);
   if (mask) mask = 0x80;
@@ -33,10 +32,10 @@ static int encode(lua_State *L) {
 
   uint32_t i;
 
-  // TODO: other than 1 types
+  /* TODO: other types? */
   p[0] = 0x80 | 0x01;
 
-  // compose header
+  /* compose header */
   if (len < 126) {
     p[1] = mask | len;
     p += 2;
@@ -55,19 +54,19 @@ static int encode(lua_State *L) {
     p += 10;
   }
 
-  // mask the buffer content?
+  /* mask the buffer content? */
   if (mask) {
 
-    // create mask
+    /* create mask */
     uint32_t ki;
     uint8_t *key = p;
-    // TODO: srand? or read /dev/urandom?
+    /* TODO: srand? or read /dev/urandom? */
     for (ki = 0; ki < 4; ++ki) {
       key[ki] = rand() & 0xFF;
     }
     p += 4;
 
-    // mask buffer content
+    /* mask buffer content */
     for (i = 0, ki = 0; i < len; ++i) {
       p[i] ^= key[ki];
       if (++ki > 3) ki = 0;
@@ -78,14 +77,12 @@ static int encode(lua_State *L) {
   return 0;
 }
 
-static int xor32s(lua_State *L) {
+static int xor32_by_string(lua_State *L) {
 
   const char *p = luaL_checkstring(L, 1);
   const char *key = luaL_checkstring(L, 2);
-  // FIXME: checklong?
   uint32_t len = luaL_checkint(L, 3);
 
-  // xor the buffer
   uint32_t i, ki;
   for (i = 0, ki = 0; i < len; ++i) {
     ((uint8_t *)p)[i] ^= key[ki];
@@ -94,18 +91,14 @@ static int xor32s(lua_State *L) {
 
   lua_pushstring(L, p);
   return 1;
-
-  //return 0;
 }
 
-static int xor32(lua_State *L) {
+static int xor32_by_uint32(lua_State *L) {
 
   const char *p = luaL_checkstring(L, 1);
   uint32_t mask = luaL_checkint(L, 2);
-  // FIXME: checklong?
   uint32_t len = luaL_checkint(L, 3);
 
-  // xor the buffer
   const uint8_t *key = (const uint8_t *)&mask;
   uint32_t i, ki;
   for (i = 0, ki = 0; i < len; ++i) {
@@ -115,26 +108,12 @@ static int xor32(lua_State *L) {
 
   lua_pushstring(L, p);
   return 1;
-
-  //return 0;
 }
-
-int createtable(lua_State *L) {
-  int narray, nhash;
-  narray = luaL_optint(L, 1, 0);
-  nhash = luaL_optint(L, 2, 0);
-  lua_createtable(L, narray, nhash);
-  return 1;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 
 static const luaL_reg exports[] = {
   {"encode", encode},
-  {"xor32", xor32},
-  {"xor32s", xor32s},
-  {"createtable", createtable},
+  {"xor32", xor32_by_uint32},
+  {"xor32s", xor32_by_string},
   {NULL, NULL}
 };
 
@@ -143,6 +122,5 @@ LUALIB_API int luaopen_hybi10(lua_State *L) {
   lua_newtable(L);
   luaL_register(L, NULL, exports);
 
-  // return the new module
   return 1;
 }
